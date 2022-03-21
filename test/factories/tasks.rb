dd4511cd934f05cdff91677be7c5b460c1a4f5e1
Ainur Galimov <ainur.galimov@dualbootpartners.com>
2022-03-21 22:48:53 +0500

    Add Board and Session controllers

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     description { 'MyString' }
     author factory: :manager
     assignee factory: :developer
-    state { 'MyString' }
     expired_at { Time.now + 1.day }
   end
 end
