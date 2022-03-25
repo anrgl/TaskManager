ca869932498ef9aaa3267d169d043e7dc2273466
Ainur Galimov <ainur.galimov@dualbootpartners.com>
2022-03-25 17:27:53 +0500

    Add tasks to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@localhost')
+puts 'Creating admin'
+admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 'admin@email.com')
 admin.password = 'admin'
 admin.save
 
+puts 'Creating managers and developers'
 60.times do |i|
   u = [Manager, Developer].sample.new
   u.email = "email#{i}@mail.gen"
@@ -11,4 +13,14 @@ admin.save
   u.last_name = "LN#{i}"
   u.password = i.to_s
   u.save
+end
+
+puts 'Creating tasks'
+20.times do |i|
+  t = Task.new
+  t.author = Manager.all.sample
+  t.assignee = Developer.all.sample
+  t.name = "TN#{i}"
+  t.description = "TD#{i}"
+  t.save
 end
